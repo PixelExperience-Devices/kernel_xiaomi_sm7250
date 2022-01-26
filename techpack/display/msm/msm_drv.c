25109f775406b90ac613ef497cd7cefb0f7f1eb6
Park Ju Hyung <qkrwngud825@gmail.com>
2022-01-26 11:10:08 +0100

    drm/msm: use kmem_cache pool for struct vblank_work
    These get allocated and freed millions of times on this kernel tree.
    
    Use a dedicated kmem_cache pool and avoid costly dynamic memory allocations.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- techpack/display/msm/msm_drv.c
+++ techpack/display/msm/msm_drv.c
@@ -64,6 +64,8 @@
 
 static DEFINE_MUTEX(msm_release_lock);
 
+static struct kmem_cache *kmem_vblank_work_pool;
+
 #define IDLE_ENCODER_MASK_DEFAULT	1
 #define IDLE_TIMEOUT_MS_DEFAULT		100
 
@@ -322,7 +324,7 @@ static void vblank_ctrl_worker(struct kthread_work *work)
 	else
 		kms->funcs->disable_vblank(kms, priv->crtcs[cur_work->crtc_id]);
 
-	kfree(cur_work);
+ 	kmem_cache_free(kmem_vblank_work_pool, cur_work);
 }
 
 static int vblank_ctrl_queue_work(struct msm_drm_private *priv,
@@ -335,7 +337,7 @@ static int vblank_ctrl_queue_work(struct msm_drm_private *priv,
 	if (!priv || crtc_id >= priv->num_crtcs)
 		return -EINVAL;
 
-	cur_work = kzalloc(sizeof(*cur_work), GFP_ATOMIC);
+ 	cur_work = kmem_cache_zalloc(kmem_vblank_work_pool, GFP_ATOMIC);
 	if (!cur_work)
 		return -ENOMEM;
 
@@ -2283,6 +2285,7 @@ static int __init msm_drm_register(void)
 		return -EINVAL;
 
 	DBG("init");
+ 	kmem_vblank_work_pool = KMEM_CACHE(vblank_work, SLAB_HWCACHE_ALIGN | SLAB_PANIC);
 	msm_smmu_driver_init();
 	msm_dsi_register();
 	msm_edp_register();
